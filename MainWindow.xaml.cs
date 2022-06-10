058acb7d6db7e71ec27d0bc44d8a97e05b3d2681
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2022-06-10 12:56:19 +0300

    add async

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Net;
 using System.Windows.Shapes;
 
 namespace WPF_Async_Demo
@@ -24,5 +25,115 @@ namespace WPF_Async_Demo
         {
             InitializeComponent();
         }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {   
+            executeSync.IsEnabled= false;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownLoadSync();
+
+            watch.Stop();
+            double elapsedS = watch.ElapsedMilliseconds / 1000.0;
+
+            resultSync.Text += $"Total exec time: {elapsedS} seconds";
+            executeSync.IsEnabled = true;
+
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> websites = new List<string>();
+
+
+            websites.Add("https://www.yahoo.com");
+            websites.Add("https://www.google.com");
+            websites.Add("https://www.stackoverflow.com");
+            websites.Add("https://www.icrontech.com");
+
+            return websites;
+        }
+
+        private void RunDownLoadSync()
+        {
+            resultSync.Text = "";
+
+            var websites = PrepData();
+            //Console.WriteLine(websites);
+
+            foreach (var site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+               // Console.WriteLine(results.WebsiteData.Length);
+                resultSync.Text += $"{results.WebsiteUrl} downloaded: {results.WebsiteData.Length} characters long. \n";
+            } 
+
+        }
+
+        private WebsiteDataModel DownloadWebsite(string siteUrl)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = siteUrl;
+            output.WebsiteData = client.DownloadString(siteUrl);
+            return output;
+        }
+
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            resultAsync.Text = "";
+
+            await RunDownLoadParallelAsync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultAsync.Text += $"Total exec time: {elapsedMs}";
+        }
+
+        private async Task RunDownLoadAsync()
+        {
+
+            var websites = PrepData();
+            //Console.WriteLine(websites);
+
+            foreach ( var site in websites)
+            {
+                WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                // Console.WriteLine(results.WebsiteData.Length);
+                resultAsync.Text += $"{results.WebsiteUrl} downloaded: {results.WebsiteData.Length} characters long. \n";
+            }
+
+        }
+        private async Task RunDownLoadParallelAsync()
+        {
+
+            var websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            //Console.WriteLine(websites);
+
+            foreach ( var site in websites)
+            {
+
+                tasks.Add(Task.Run(() => DownloadWebsite(site)));
+
+                //WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                // Console.WriteLine(results.WebsiteData.Length);
+                //resultAsync.Text += $"{results.WebsiteUrl} downloaded: {results.WebsiteData.Length} characters long. \n";
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var item in results) 
+            {
+                resultAsync.Text += $"{item.WebsiteUrl} downloaded: {item.WebsiteData.Length} characters long. \n";
+
+            }
+
+        }
+
     }
 }
---
+++ WebsiteDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_Async_Demo
+{
+    public class WebsiteDataModel
+    {
+
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
